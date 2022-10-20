0cb3cb738143c578035cb832c10a72fa17c71a4b
Naif aslan <naifasln3121@gmail.com>
2022-10-20 19:28:26 +0300

    naif

---
+++ src/test/java/tests/naif/US003_TC01.java
@@ -0,0 +1,16 @@
+package tests.naif;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AlloverPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US003_TC01 {
+
+
+}
+
+
